82f49b695ba8f2776c98f0830a67469ba633955b
Kaushal Kumar <kaushal.k@intelegencia.com>
2017-12-28 22:55:48 +0530

    real time data binding

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-about',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  usersObservable: Observable<any[]>;
+  constructor(private db: AngularFireDatabase) { }
   ngOnInit() {
+    this.usersObservable = this.getUsers('/users');
   }
-
+  getUsers(listPath): Observable<any[]> {
+    return this.db.list(listPath).valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+// import * as firebase from 'firebase';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,19 +16,31 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCy9HuK8-DkBdZWiJrwPSCscOoWJmBqLQ8',
+  authDomain: 'logindemo-23f28.firebaseapp.com',
+  databaseURL: 'https://logindemo-23f28.firebaseio.com',
+  projectId: 'logindemo-23f28',
+  storageBucket: 'logindemo-23f28.appspot.com',
+  messagingSenderId: '70784148192'
+};
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     FormsModule
   ],
   providers: [],
