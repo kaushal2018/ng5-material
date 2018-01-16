b17cc90768fab5b6e4e000061352da58c40e85cf
Kaushal Kumar <kaushal.k@intelegencia.com>
2018-01-16 13:54:02 +0530

    using ngModel in TD forms

--- src/app/components/signup2/signup2.component.ts
+++ src/app/components/signup2/signup2.component.ts
@@ -13,14 +13,16 @@ export class Signup2Component implements OnInit {
   // https://toddmotto.com/angular-2-forms-template-driven
   // public user: User; // user model using user interface
   // user = new UserModel(); // user model using user class
-  tempUser: UserModel[] = [];
+
+  // tempUser: UserModel[] = [];
+
   // userdetails;
   // constructor(private aservice: AuthorListService) {
   //  this.userdetails =  aservice.resetuserModel(this.user);
   //  console.log(this.userdetails);
   // }
-  user: UserModel = {
-    userid: 123,
+  user = [{
+    userid: null,
     password: '',
     username: '',
     email: '',
@@ -28,7 +30,7 @@ export class Signup2Component implements OnInit {
       landline: '',
       mobile: ''
     }
-  };
+  }];
   constructor() {
     // this.user = {
     //   userid: null,
@@ -44,7 +46,7 @@ export class Signup2Component implements OnInit {
 
   addUser(formData: any): void {
     if (formData.valid) {
-      this.tempUser = [{
+      this.user = [{
         userid: formData.value.userid,
         password: formData.value.password,
         username: formData.value.username,
@@ -57,6 +59,22 @@ export class Signup2Component implements OnInit {
       console.log(this.user);
     }
   }
+
+  // addUser(formData: any): void {
+  //   if (formData.valid) {
+  //     this.tempUser = [{
+  //       userid: formData.value.userid,
+  //       password: formData.value.password,
+  //       username: formData.value.username,
+  //       email: formData.value.email,
+  //       phone: {
+  //         landline: formData.value.phone.landline,
+  //         mobile: formData.value.phone.mobile
+  //       }
+  //     }];
+  //     console.log(this.user);
+  //   }
+  // }
 
   ngOnInit() {
     console.log(this.user);
--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,23 +1,23 @@
+export class UserModel {
+    userid: number;
+    password: string;
+    username: string;
+    email: string;
+    phone: {
+        landline: string,
+        mobile: string
+    };
+}
+
 // export class UserModel {
-//     userid: number;
-//     password: string;
-//     username: string;
-//     email: string;
-//     phone: {
-//         landline: string,
-//         mobile: string
-//     };
+//     userid: number = 123;
+//     password: string = '';
+//     username: string = '';
+//     email: string = '';
+//     phone: PhoneModel;
 // }
 
-export class UserModel {
-    userid: number = 123;
-    password: string = '';
-    username: string = '';
-    email: string = '';
-    phone: PhoneModel;
-}
-
-export class PhoneModel {
-    landline: string = '';
-    mobile: string = '';
-}
+// export class PhoneModel {
+//     landline: string = '';
+//     mobile: string = '';
+// }
